03b0e6bca442d01772b668e9f7a3ff66068bde63
Rafael Hernandez <hck3lool@gmail.com>
2024-02-26 19:12:26 -0500

    Add reto 2

--- Lite_Thinking_Retos/junit/src/test/java/apiRest/apiRestTest.java
+++ Lite_Thinking_Retos/junit/src/test/java/apiRest/apiRestTest.java
@@ -12,6 +12,7 @@ import java.time.Duration;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +24,9 @@ public class apiRestTest {
 
     @BeforeEach
     public void setup(){
-        System.out.println("Inicio de la Configuracion de la Prueba REST API");
+        System.out.println("------------------------------------------------");
+        System.out.println("Inicio de la Prueba REST API");
+        System.out.println("------------------------------------------------");
         clientHttp = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
             .connectTimeout(Duration.ofSeconds(10))
@@ -36,10 +39,8 @@ public class apiRestTest {
         URIBuilder uri = new URIBuilder()
             .setScheme("https")
             .setHost("reqres.in")
-            .setPath("api/users")
+            .setPath("/api/users")
             .addParameter("page", "2");
-            // .addParameter("api_key", "7zmkWLzoIo4pCeApBazRH0gmrtu79131G0yNDtDO")
-            // .addParameter("date", "2023-05-27");
 
         System.out.println("Endpoint: " + uri.build());
 
@@ -53,12 +54,139 @@ public class apiRestTest {
         System.out.println("Response: " + response.body());
 
         assertEquals(HttpStatus.SC_OK, response.statusCode());
-        // assertTrue(response.body().contains("\"title\":\"Crescent Neptune and Triton\""));
+        assertTrue(response.body().contains("\"total\":12"));
 
-        // String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "ApodSchema.json");
+        String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "ConsultarUsuariosSchema.json");
 
-        // System.out.println("Resultado de la validacion del Schema: " + validationSchema);
+        System.out.println("Resultado de la validacion del Schema: " + validationSchema);
 
-        // assertEquals("", validationSchema, "Resultado de la validacion del schema");
+        assertEquals("", validationSchema, "Resultado de la validacion del schema");
+    }
+
+    @Test
+    public void consultarDatos() throws URISyntaxException, IOException, InterruptedException{
+
+        URIBuilder uri = new URIBuilder()
+            .setScheme("https")
+            .setHost("reqres.in")
+            .setPath("api/users")
+            .addParameter("page", "2")
+            .addParameter("id", "12");
+
+        System.out.println("Endpoint: " + uri.build());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(uri.build())
+            .build();
+
+        HttpResponse<String> response = clientHttp.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Response: " + response.body());
+
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertTrue(response.body().contains("\"first_name\":\"Rachel\""));
+
+        String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "ConsultarDatosSchema.json");
+
+        System.out.println("Resultado de la validacion del Schema: " + validationSchema);
+
+        assertEquals("", validationSchema, "Resultado de la validacion del schema");
+    }
+
+    @Test
+    public void consultarUsuarioInexistente() throws URISyntaxException, IOException, InterruptedException{
+
+        URIBuilder uri = new URIBuilder()
+            .setScheme("https")
+            .setHost("reqres.in")
+            .setPath("api/users")
+            .addParameter("id", "0");
+
+        System.out.println("Endpoint: " + uri.build());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(uri.build())
+            .build();
+
+        HttpResponse<String> response = clientHttp.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Response: " + response.body());
+
+        assertEquals(HttpStatus.SC_NOT_FOUND, response.statusCode());
+        assertTrue(response.body().contains(""));
+
+        String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "ConsultarUsuarioInexistenteSchema.json");
+
+        System.out.println("Resultado de la validacion del Schema: " + validationSchema);
+
+        assertEquals("", validationSchema, "Resultado de la validacion del schema");
+    }
+
+    @Test
+    public void consultarApiUnknow() throws URISyntaxException, IOException, InterruptedException{
+
+        URIBuilder uri = new URIBuilder()
+            .setScheme("https")
+            .setHost("reqres.in")
+            .setPath("api/unknow");
+
+        System.out.println("Endpoint: " + uri.build());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(uri.build())
+            .build();
+
+        HttpResponse<String> response = clientHttp.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Response: " + response.body());
+
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertTrue(response.body().contains("\"name\":\"blue turquoise\""));
+
+        String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "ConsultarApiUnknowSchema.json");
+
+        System.out.println("Resultado de la validacion del Schema: " + validationSchema);
+
+        assertEquals("", validationSchema, "Resultado de la validacion del schema");
+    }
+
+    @Test
+    public void delayedResponse() throws URISyntaxException, IOException, InterruptedException{
+
+        URIBuilder uri = new URIBuilder()
+            .setScheme("https")
+            .setHost("reqres.in")
+            .setPath("/api/users")
+            .addParameter("delay", "3");
+
+        System.out.println("Endpoint: " + uri.build());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(uri.build())
+            .build();
+
+        HttpResponse<String> response = clientHttp.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Response: " + response.body());
+
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertTrue(response.body().contains("\"avatar\":\"https://reqres.in/img/faces/3-image.jpg\""));
+
+        String validationSchema =  JsonSchemaValidator.validateJsonAgainstSchema(response.body(), "delayedResponseSchema.json");
+
+        System.out.println("Resultado de la validacion del Schema: " + validationSchema);
+
+        assertEquals("", validationSchema, "Resultado de la validacion del schema");
+    }
+
+    @AfterEach
+    public void teardown(){
+        System.out.println("------------------------------------------------");
+        System.out.println("Fin de la Prueba REST API");
+        System.out.println("------------------------------------------------\n");
     }
 }
